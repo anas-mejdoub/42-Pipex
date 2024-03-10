d53d564b4e18d674c1ded67922153e8b7a40d83b
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-03-10 17:50:12 +0000

    solving norminette problems

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:32:23 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/03/10 13:25:30 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/03/10 17:49:45 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
 #include <unistd.h>
 #include "libft/libft.h"
 #include <errno.h>
+
+typedef struct s_variables
+{
+    int fdin;
+    int fdout;
+	int **fd;
+	int *pids;
+	pid_t pid;
+} variables;
+typedef struct s_commands_variables
+{
+  	char	*path;
+	char	**command_args;
+} commands_;
 
 int count_word(char *str)
 {
@@ -195,7 +209,6 @@ char *find_path(char *command, char *envp)
 }
 void	exit_error(char *str, int n)
 {
-	// if ((i >= argc - 2 && c == 'P') || (i < argc - 2))
 	perror(str);
 	exit(n);
 }
@@ -221,22 +234,16 @@ void start_pipe(int fdin, int fd)
 {
 	dup2(fdin, STDIN_FILENO);
 	dup2(fd, STDOUT_FILENO);
-	// close();
-	// close();
 }
 void middle_pipe(int fd1, int fd2)
 {
 	dup2(fd1, STDIN_FILENO);
 	dup2(fd2, STDOUT_FILENO);
-	// close(STDIN_FILENO);
-	// close(STDOUT_FILENO);
 }
 void last_pipe(int fdout, int fd1)
 {
 	dup2(fd1, STDIN_FILENO);
 	dup2(fdout, STDOUT_FILENO);
-	// close(STDIN_FILENO);
-	// close(STDOUT_FILENO);
 }
 pid_t smart_fork()
 {
@@ -251,142 +258,128 @@ pid_t smart_fork()
 	else
 		return (pid);
 }
-int	smart_pipe(int fd[2])
-{
-	int	res;
 
-	res = pipe(fd);
-	if (res == -1)
+
+
+
+void close_fd(int **fd, int n)
+{
+	int	i;
+	
+	i = 0;
+	while (i < n)
 	{
-		perror("pipe");
-		exit(-1);
+		close(fd[i][0]);
+		close(fd[i][1]);
+		i++;
 	}
+}
+int **malloc_2d(int **fd, int rows)
+{
+	int	i;
+
+	i = 0;
+	fd = malloc(rows * sizeof(int *));
+	if (fd == NULL)
+		return (NULL);
+	while (i < rows)
+	{
+		fd[i] = malloc(2 * sizeof(int));
+		if (fd[i] == NULL)
+			return (NULL);
+		i++;
+	}
+	return (fd);
+}
+int *malloc_int_arr(int *fd, int n)
+{
+	fd = malloc(n * sizeof(int));
+	if (fd == NULL)
+		return (NULL);
+	return (fd);
+}
+void smart_pipe(int **fd, int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < n)
+	{
+		j = pipe(fd[i]);
+		if (j == -1)
+			exit_error("PIPE", 1);
+		i++;
+	}
+}
+void pipes_level(int argc, int i, variables var ,commands_ command, char *envp[])
+{
+	if (i == 2)
+		start_pipe(var.fdin, var.fd[i - 2][1]);
+	else if (i < argc - 2)
+		middle_pipe(var.fd[i - 3][0], var.fd[i - 2][1]);
 	else
-		return (res);
+		last_pipe(var.fdout, var.fd[i - 3][0]);
+	close_fd(var.fd, argc - 4);
+	if (execve(command.path, command.command_args, envp) == -1)
+				exit_error(command.command_args[0], 127);
+}
+void parent_helper(variables var, int argc, int *status)
+{
+	int	i;
+
+	close_fd(var.fd, argc - 4);
+	i = 0;
+	while (i < argc - 3)
+	{
+		waitpid(var.pids[i], status, 0);
+		i++;
+	}
+}
+int helper_function(variables var, int argc, char  *argv[], char *envp[])
+{
+	commands_ command;
+	int		i;
+	int		status;
+
+	i = 2;
+	command.command_args = NULL;
+	command.path = NULL;
+	while (i < argc - 1)
+	{
+		free2d(command.command_args);
+		free(command.path);
+		command.command_args = optionSplit((char *)argv[i]);
+		command.path = find_path(command.command_args[0], get_env(envp));
+		var.pid = fork();
+		if (var.pid == -1)
+			perror("FORK ");
+		if (var.pid == 0)
+			pipes_level(argc, i, var, command, envp);
+		else
+			var.pids[i - 2] = var.pid;
+		i++;
+	}
+	if (var.pid > 0)
+		parent_helper(var, argc, &status);
+	return (status);
 }
 
-// void helper_function(int fdin, int fdout, int argc, char  *argv[], char *envp[])
-// {
-// 	char	*path;
-// 	int		fd[argc - 3][2];
-// 	char	**command_args;
-// 	int		i;
-// 	int		status;
-// 	pid_t	pid;
-	
-// 	i = 2;
-// 	command_args = NULL;
-// 	path = NULL;
-// 	while (i < argc - 1)
-// 	{
-// 		smart_pipe(fd[i - 2]);
-// 		free2d(command_args);
-// 		free(path);
-// 		command_args = optionSplit((char *)argv[i]);
-// 		path = find_path(command_args[0], get_env(envp));
-// 		pid = smart_fork();
-// 		if (pid == 0)
-// 		{
-// 			if (i == 2)
-// 				start_pipe(fdin, fd[i - 2][1]);
-// 			else if (i < argc - 2)
-// 				middle_pipe(fd[i - 3][0], fd[i - 2][1]);
-// 			else
-// 				last_pipe(fdout, fd[i - 3][0]);
-// 			if (execve(path, command_args, envp) == -1)
-// 				exit_error(command_args[0], 127, i, argc, 'C');
-// 		}
-// 		else if (pid > 0)
-// 		{
-// 			waitpid(pid, &status, 0);
-// 			if (WEXITSTATUS(status) != 0 && i >= argc - 2)
-// 				exit_error(command_args[0], 127, i, argc, 'P');
-// 		}
-// 		close(fd[i - 2][1]);
-// 		i++;
-// 	}
-// }
 int main(int argc, char  *argv[], char *envp[])
 {
-		char	*path;
-		int		fd[argc - 4][2];
-		int		pids[argc - 3];
-		int		fdin;
-		int		fdout;
-		char	**command_args;
-		int		i;
 		int		status;
-		int		j;
-		pid_t	pid;
-
-		i = 2;
-		command_args = NULL;
-		path = NULL;
+		variables var;
+		
 		if (argc >= 4)
 		{
-			fdout = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0644);
-			fdin = open(argv[1], O_RDWR);
-			if (fdout == -1 || fdin == -1)
+			var.fd = malloc_2d(var.fd, argc - 4);
+			var.pids = malloc_int_arr(var.pids, argc - 3);
+			var.fdout = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0644);
+			var.fdin = open(argv[1], O_RDWR);
+			if (var.fdout == -1 || var.fdin == -1)
 				exit_error("FILE", 1);
-			i = 0;
-			while (i < argc - 4)
-			{
-				j = pipe(fd[i]);
-				if (j == -1)
-					exit_error("PIPE", 1);
-				i++;
-			}
-			i = 2;
-			// helper_function(fdin, fdout, argc, argv, envp);
-			while (i < argc - 1)
-			{
-				free2d(command_args);
-				free(path);
-				command_args = optionSplit((char *)argv[i]);
-				path = find_path(command_args[0], get_env(envp));
-				pid = fork();
-				
-				if (pid == -1 || j == -1)
-					perror("FORK ");
-				if (pid == 0)
-				{
-					if (i == 2)
-						start_pipe(fdin, fd[i - 2][1]);
-					else if (i < argc - 2)
-						middle_pipe(fd[i - 3][0], fd[i - 2][1]);
-					else
-						last_pipe(fdout, fd[i - 3][0]);
-					int k = 0;
-					while (k < argc - 4)
-					{
-						close(fd[k][0]);
-						close(fd[k][1]);
-						k++;
-					}
-					if (execve(path, command_args, envp) == -1)
-						exit_error(command_args[0], 127);
-				}
-				else
-					pids[i - 2] = pid;
-				i++;
-			}
-			if (pid > 0)
-			{
-				i = 0;
-				while (i < argc - 4)
-				{
-					close(fd[i][0]);
-					close(fd[i][1]);
-					i++;
-				}
-				i = 0;
-				close(fdin);
-				while (i < argc - 3)
-				{
-					waitpid(pids[i], &status, 0);
-					i++;
-				}
-			}
+			smart_pipe(var.fd, argc - 4);
+			status = helper_function(var, argc, argv, envp);
 		}
 	return (WEXITSTATUS(status));
 }
